91017ffbf8b0a9dc2a1fed9c74d4aa7a6b507b15
marmoh2002 <mhabiba2002@gmail.com>
2024-08-27 15:20:08 +0300

    added more benchmark functions

--- funcs.py
+++ funcs.py
@@ -34,3 +34,16 @@ def schwefel(x):
     """Schwefel function"""
     n = len(x)
     return 418.9829 * n - np.sum(x * np.sin(np.sqrt(np.abs(x))))
+def levy(x):
+    """Levy function"""
+    w = 1 + (x - 1) / 4
+    term1 = np.sin(np.pi * w[0])**2
+    term2 = np.sum((w[:-1] - 1)**2 * (1 + 10 * np.sin(np.pi * w[:-1] + 1)**2))
+    term3 = (w[-1] - 1)**2 * (1 + np.sin(2 * np.pi * w[-1])**2)
+    return term1 + term2 + term3
+def michalewicz(x):
+    """Michalewicz function"""
+    m = 10  # steepness factor
+    d = len(x)
+    i = np.arange(1, d + 1)
+    return -np.sum(np.sin(x) * np.sin(i * x**2 / np.pi)**(2 * m))
